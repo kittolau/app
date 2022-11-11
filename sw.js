2925c403721e7e3c954d016dc3aa29656040c609
Kitto Lau <kittolau4649@gmail.com>
2022-11-11 17:33:25 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,31 @@
+// Files to cache
+const cacheName = 'kitto-app-v1';
+const contentToCache = [
+  '/',
+  '/index.html',
+  '/icon-40.png',
+];
+
+// Installing Service Worker
+self.addEventListener('install', (e) => {
+  console.log('[Service Worker] Install');
+  e.waitUntil((async () => {
+    const cache = await caches.open(cacheName);
+    console.log('[Service Worker] Caching all: app shell and content');
+    await cache.addAll(contentToCache);
+  })());
+});
+
+// Fetching content using Service Worker
+self.addEventListener('fetch', (e) => {
+  e.respondWith((async () => {
+    const r = await caches.match(e.request);
+    console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+    if (r) return r;
+    const response = await fetch(e.request);
+    const cache = await caches.open(cacheName);
+    console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+    cache.put(e.request, response.clone());
+    return response;
+  })());
+});
